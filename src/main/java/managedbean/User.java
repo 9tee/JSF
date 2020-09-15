b7cb20dfe40618b3f696252e92ad4b63f6f96244
Nam Nguyen <sonnam99@gmail.com>
2020-09-15 14:48:17 +0700

    init

---
+++ src/main/java/managedbean/User.java
@@ -0,0 +1,22 @@
+package managedbean;
+
+Facelet Title  
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+
+
+@ManagedBean
+@RequestScoped
+public class User {
+
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
